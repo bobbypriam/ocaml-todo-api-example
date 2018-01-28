f4a3f0a09a18fbc214fcec713e11216929971ce3
Bobby Priambodo <bobby.priambodo@gmail.com>
2018-01-28 19:09:17 +0700

    Refactor: extract request handlers

--- src/server.ml
+++ src/server.ml
@@ -1,39 +1,41 @@
 open Opium.Std
+open Lwt.Infix
 
 let index_page = get "/" (fun _req -> `String "Hello TodoDB\n" |> respond')
 
-let todos_list =
-  get "/todos" (fun _req ->
+let todos_list_handler () =
+  let open Ezjsonm in
+  match%lwt Todos.get_all () with
+  | Ok todos ->
+    let result_json =
+      list (fun (todo : Todos.todo) ->
+          dict [("id", int todo.id); ("content", string todo.content)]
+        ) todos in
+    Lwt.return @@ `Json result_json
+  | Error Todos.Database_error ->
+    Lwt.return @@ `Json (dict [("error", string "Database error.")])
+
+let todos_add_handler req =
+  App.json_of_body_exn req >>= (fun json ->
       let open Ezjsonm in
-      match%lwt Todos.get_all () with
-      | Ok todos ->
-        let result_json =
-          list (fun (todo : Todos.todo) ->
-              dict [("id", int todo.id); ("content", string todo.content)]
-            ) todos in
-        `Json result_json |> respond'
+      let json_value = value json in
+      let content = get_string (find json_value ["content"]) in
+      match%lwt Todos.add content with
+      | Ok () ->
+        Lwt.return @@ `Json (dict [ ("ok", bool true) ])
       | Error Todos.Database_error ->
-        `Json (dict [("error", string "Database error.")]) |> respond'
+        Lwt.return @@ `Json (dict [("error", string "Database error.")])
     )
 
-let todos_add =
-  post "/todos" (fun req ->
-      let promise = req |> App.json_of_body_exn in
-      Lwt.bind promise (fun json ->
-          let open Ezjsonm in
-          let json_value = value json in
-          let content = get_string (find json_value ["content"]) in
-          match%lwt Todos.add content with
-          | Ok () ->
-            `Json (dict [ ("ok", bool true) ]) |> respond'
-          | Error Todos.Database_error ->
-            `Json (dict [("error", string "Database error.")]) |> respond'
-        )
-    )
+let todos_list_route =
+  get "/todos" (fun _req -> todos_list_handler () >>= respond')
+
+let todos_add_route =
+  post "/todos" (fun req -> todos_add_handler req >>= respond')
 
 let start () =
   App.empty
   |> index_page
-  |> todos_list
-  |> todos_add
+  |> todos_list_route
+  |> todos_add_route
   |> App.run_command
